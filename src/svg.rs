ffbcdb4655a803f1f3e114df55191a432496ccc4
tukinami seika <10forchette@gmail.com>
2024-04-01 19:00:58 +0900

    modify: update libraries.

--- src/svg.rs
+++ src/svg.rs
@@ -1,7 +1,6 @@
 use resvg::{
     tiny_skia::{Pixmap, Transform},
-    usvg::{Options, Tree, TreeParsing, TreeTextToPath},
-    FitTo,
+    usvg::{Options, Tree},
 };
 
 use crate::error::PieChartError;
@@ -11,22 +10,19 @@ where
     T: AsRef<str>,
 {
     let mut options = Options::default();
-    let fit_to = FitTo::Original;
-
-    let mut fontdb = resvg::usvg::fontdb::Database::new();
-    fontdb.load_system_fonts();
     options.languages.push("ja".to_string());
     options.font_family = String::from("ＭＳ ゴシック");
 
-    let mut tree = Tree::from_str(svg_src.as_ref(), &options)?;
-    tree.convert_text(&fontdb);
+    let mut fontdb = resvg::usvg::fontdb::Database::new();
+    fontdb.load_system_fonts();
 
-    let size = fit_to
-        .fit_to(tree.view_box.rect.to_screen_size())
+    let tree = Tree::from_str(svg_src.as_ref(), &options, &fontdb)?;
+
+    let viewbox_rect = tree.view_box().rect;
+    let mut pixmap = Pixmap::new(viewbox_rect.width() as u32, viewbox_rect.height() as u32)
         .ok_or(PieChartError::SizeTooSmall)?;
-    let mut pixmap = Pixmap::new(size.width(), size.height()).ok_or(PieChartError::SizeTooSmall)?;
 
-    resvg::render(&tree, fit_to, Transform::default(), pixmap.as_mut());
+    resvg::render(&tree, Transform::default(), &mut pixmap.as_mut());
 
     Ok(pixmap)
 }
